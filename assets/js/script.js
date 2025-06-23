a5f9a3f02b4c558088b64d5075dbb219498eccb4
AndyV773 <ajv103@hotmail.com>
2025-06-23 13:30:50 +0100

    add: hero slide show and intro section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,42 @@
-// updates the copyright section to the most recent year
-document.getElementById("year").textContent = new Date().getFullYear();
+let slideIndex = 0;
+let hero = document.getElementById("hero");
+
+function showSlideShow() {
+    let i;
+    let dots = document.getElementsByClassName("dot");
+
+    // Array of background images
+    const backgroundImages = [
+        "url('assets/images/img01.jpg')",
+        "url('assets/images/img02.jpg')",
+        "url('assets/images/img03.jpg')"
+    ];
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active-dot", "");
+    }
+
+    slideIndex++;
+
+    if (slideIndex > backgroundImages.length) {
+        slideIndex = 1;
+    }
+
+    hero.style.backgroundImage = backgroundImages[slideIndex - 1];
+
+    dots[slideIndex - 1].className += " active-dot";
+
+    // Change image every 2 seconds
+    setTimeout(showSlideShow, 3000);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    // updates the copyright section to the most recent year
+    document.getElementById("year").textContent = new Date().getFullYear();
+
+   
+    if (hero) {
+        showSlideShow();
+    }
+});
